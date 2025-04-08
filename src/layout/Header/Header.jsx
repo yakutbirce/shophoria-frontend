6067e7d2c3dcaaf1eb24be0a6835c04e2df5c668
Birçe Yakut <yakutbirce@gmail.com>
2025-04-08 14:45:03 +0300

    feat: Complete mobile header layout (navbar + hero section)

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -1,9 +1,11 @@
 import MobileHeader from "./MobileHeader";
+import SummerCollection from "./SummerCollection";
 
 function Header() {
   return (
     <header className="block lg:hidden">
       <MobileHeader />
+      <SummerCollection />  
     </header>
   );
 }
