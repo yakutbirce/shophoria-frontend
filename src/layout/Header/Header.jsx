b382c1a48db2d7f6caf802839ddc8020ce4930c1
Birçe Yakut <yakutbirce@gmail.com>
2025-04-09 00:03:10 +0300

    Add desktop navbar implementation

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -1,12 +1,22 @@
+import DesktopNavbar from "./DesktopNavbar"; 
 import MobileHeader from "./MobileHeader";
 import SummerCollection from "./SummerCollection";
 
 function Header() {
   return (
-    <header className="block lg:hidden">
-      <MobileHeader />
-      <SummerCollection />  
-    </header>
+    <>
+      {/* Mobile Header */}
+      <header className="block lg:hidden">
+        <MobileHeader />
+        <SummerCollection />
+      </header>
+
+      {/* Desktop Header */}
+      <header className="hidden lg:block bg-white">
+        <DesktopNavbar />
+        <SummerCollection />
+      </header>
+    </>
   );
 }
 
